a4d5c1156e450a2a1013612cdd96054ac222eb01
SornchaiTheDev <sornchaithedev@gmail.com>
2025-04-21 00:07:23 +0700

    config: add with compilers version

--- container-extra-config.sh
+++ container-extra-config.sh
@@ -3,22 +3,19 @@
 # Disable swap memory
 swapoff -a
 
-# Disable CPU frequency scaling
-echo off > /sys/devices/system/cpu/smt/control
-
 # Disable address space layout randomization
-echo 0 > /proc/sys/kernel/randomize_va_space
+echo 0 > /proc/sys/kernel/randomize_va_space 2> /dev/null
 
 # Disable transparent huge pages (THP)
-echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag 2> /dev/null
 
 # Disable THP defragmentation
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo never > /sys/kernel/mm/transparent_hugepage/defrag 2> /dev/null
 
 # Disable THP in general
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/enabled 2> /dev/null
 
 # Disable CPU frequency scaling
-echo core >/proc/sys/kernel/core_pattern
+echo core > /proc/sys/kernel/core_pattern 2> /dev/null
 
 /bin/bash -c "$@"
